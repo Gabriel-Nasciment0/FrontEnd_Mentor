1a299f4efcb7615988ded57aa115a6735719fc19
Gabriel <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2025-08-31 15:09:50 -0300

    .

--- faq-accordion-main/script/script.js
+++ faq-accordion-main/script/script.js
@@ -3,9 +3,19 @@ let buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
     button.addEventListener("click", (event) => {
         event.preventDefault();
+
         const question = button.parentElement;
+
         const answer = question.nextElementSibling;
 
         answer.classList.toggle("open");
+
+        const img = button.querySelector("img");
+
+        if (answer.classList.contains("open")) {
+            img.src = "assets/images/icon-minus.svg";
+        } else {
+            img.src = "assets/images/icon-plus.svg";
+        }
     });
 });
